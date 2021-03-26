3125538205c1cced25d6b846de4b72dfed5bec07
cahaseler <craig.haseler@hq.doe.gov>
2021-03-26 09:34:16 -0400

    Removed default directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,14 +17,13 @@ if [ -z "$INPUT_SHARE_NAME" ]; then
   exit 1
 fi
 
-$FILES="/"
 if [ -z "$INPUT_SOURCE_DIR" ]; then
-  echo "Source name is not set. Running on entire repo."
-  $FILES=$INPUT_SOURCE_DIR
+   echo "Source Dir name is not set. Quitting."
+  exit 1
 fi
 
 az storage file upload-batch \
   --account-name $INPUT_ACCOUNT_NAME
   --account-key $INPUT_ACCOUNT_KEY
   --destination $INPUT_SHARE_NAME
-  --source $FILES
+  --source $INPUT_SOURCE_DIR
