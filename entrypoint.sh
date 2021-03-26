9ac32daf74ef82bdaf961131cfe072a31f24095e
cahaseler <craig.haseler@hq.doe.gov>
2021-03-26 09:01:40 -0400

    Updated to make use of upload-batch command

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,19 +17,14 @@ if [ -z "$INPUT_SHARE_NAME" ]; then
   exit 1
 fi
 
-
-$FILES=/
+$FILES="/"
 if [ -z "$INPUT_SOURCE_DIR" ]; then
   echo "Source name is not set. Running on entire repo."
   $FILES=$INPUT_SOURCE_DIR
 fi
 
-for f in $FILES
-do
-  echo "Uploading $f..."
-  az storage file upload \
-    --account-name $INPUT_ACCOUNT_NAME
-    --account-key $INPUT_ACCOUNT_KEY
-    --destination $INPUT_SHARE_NAME
-    --source $FILES
-done
+az storage file upload-batch \
+  --account-name $INPUT_ACCOUNT_NAME
+  --account-key $INPUT_ACCOUNT_KEY
+  --destination $INPUT_SHARE_NAME
+  --source $FILES
